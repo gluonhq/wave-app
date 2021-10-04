a7429ec9b9f1e08db6fdb09a3ce637c89409ff5f
Johan Vos <johan@lodgon.com>
2021-10-04 10:23:07 +0200

    add avatar

--- App/src/main/java/com/gluonhq/chat/views/ChannelCell.java
+++ App/src/main/java/com/gluonhq/chat/views/ChannelCell.java
@@ -3,9 +3,18 @@ package com.gluonhq.chat.views;
 import com.gluonhq.charm.glisten.control.CharmListCell;
 import com.gluonhq.charm.glisten.control.ListTile;
 import com.gluonhq.chat.model.Channel;
+import com.gluonhq.chat.model.User;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ChannelCell extends CharmListCell<Channel> {
 
+    static final int AVATAR_SIZE = 64;
+    
     private final ListTile tile;
 
     public ChannelCell() {
@@ -18,7 +27,22 @@ public class ChannelCell extends CharmListCell<Channel> {
     @Override
     public void updateItem(Channel channel, boolean empty) {
         super.updateItem(channel, empty);
-
+        tile.setPrimaryGraphic(null);
+        if (channel.getMembers().size() > 0) {
+            User author = channel.getMembers().get(0);
+            String avatarPath = author.getAvatarPath();
+            if ((avatarPath != null) && (!avatarPath.isEmpty()) && !("null".equals(avatarPath))) {
+                try {
+                    byte[] b = Files.readAllBytes(Path.of(avatarPath));
+                    ByteArrayInputStream bais = new ByteArrayInputStream(b);
+                    Image im = new Image(bais, AVATAR_SIZE, AVATAR_SIZE, true, true);
+                    ImageView iv = new ImageView(im);
+                    tile.setPrimaryGraphic(iv);
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        }
         /* TODO: Add User Image
         final Avatar avatar = new Avatar;
         avatar.setMouseTransparent(true);
