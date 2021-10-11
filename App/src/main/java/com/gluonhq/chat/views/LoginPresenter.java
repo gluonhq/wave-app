76d6ef3786b28ace07035724ad7e131fef4a91e2
Johan Vos <johan@lodgon.com>
2021-10-11 15:31:46 +0200

    add an interface with callback methods

--- App/src/main/java/com/gluonhq/chat/views/LoginPresenter.java
+++ App/src/main/java/com/gluonhq/chat/views/LoginPresenter.java
@@ -4,6 +4,7 @@ import com.gluonhq.charm.glisten.afterburner.GluonPresenter;
 import com.gluonhq.charm.glisten.application.ViewStackPolicy;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.chat.GluonChat;
+import com.gluonhq.chat.service.BootstrapClient;
 import com.gluonhq.chat.service.Service;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
@@ -11,8 +12,9 @@ import javafx.scene.image.ImageView;
 
 import javax.inject.Inject;
 import java.util.ResourceBundle;
+import javafx.scene.image.Image;
 
-public class LoginPresenter extends GluonPresenter<GluonChat> {
+public class LoginPresenter extends GluonPresenter<GluonChat> implements BootstrapClient{
 
     @FXML private View loginView;
     @FXML private ImageView qrImageView;
@@ -33,11 +35,17 @@ public class LoginPresenter extends GluonPresenter<GluonChat> {
         }
     }
 
-    public ImageView getQrImageView() {
-        return qrImageView;
+    @Override
+    public void gotImage(Image img) {
+        this.qrImageView.setImage(img);
     }
 
-    public void nextStep() {
+    @Override
+    public void bootstrapSucceeded() {
+        nextStep();
+    }
+
+    private void nextStep() {
         AppViewManager.FIRST_VIEW.switchView(ViewStackPolicy.SKIP);
     }
 }
