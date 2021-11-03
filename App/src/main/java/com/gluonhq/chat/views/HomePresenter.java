48cbcab2c1338a44a5fee929c3c02e20138fe4de
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-11-03 09:43:08 +0530

    Initialize service in HomePresenter with Task and visual cue

--- App/src/main/java/com/gluonhq/chat/views/HomePresenter.java
+++ App/src/main/java/com/gluonhq/chat/views/HomePresenter.java
@@ -2,10 +2,16 @@ package com.gluonhq.chat.views;
 
 //import com.gluonhq.attach.orientation.OrientationService;
 import com.gluonhq.charm.glisten.application.ViewStackPolicy;
+import com.gluonhq.charm.glisten.control.ProgressIndicator;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.chat.service.Service;
 import javafx.beans.value.ChangeListener;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 
@@ -17,26 +23,26 @@ public class HomePresenter {
     @Inject private Service service;
     
     private final ChangeListener<Number> widthListener = (o, ov, nv) -> changeOrientation(nv.doubleValue());
+    private final ChangeListener<Boolean> showingListener = (obs, ov, nv) -> {
+        if (nv) {
+            homeView.getScene().widthProperty().addListener(widthListener);
+        } else {
+            homeView.getScene().widthProperty().removeListener(widthListener);
+        }
+    };
 
     public void initialize() {
 
 //        OrientationService.create().ifPresent(o -> o.orientationProperty().addListener(obs -> setupView()));
-
-        homeView.showingProperty().addListener((obs, ov, nv) -> {
-            if (nv) {
-                setupView();
-                final double width = homeView.getScene().getWidth();
-                changeOrientation(width);
-                homeView.getScene().widthProperty().addListener(widthListener);
-            } else {
-                homeView.getScene().widthProperty().removeListener(widthListener);
-            }
-        });
+        setupView();
+        homeView.showingProperty().addListener(showingListener);
     }
 
     private void setupView() {
         if (service.loggedUser() == null) {
             AppViewManager.LOGIN_VIEW.switchView(ViewStackPolicy.SKIP);
+        } else {
+            loadContacts();
         }
     }
 
@@ -48,5 +54,28 @@ public class HomePresenter {
             AppViewManager.PORTRAIT_VIEW.switchView()
                     .ifPresent(p -> ((PortraitPresenter) p).loadChat());
         }
+    }
+
+    private void loadContacts() {
+        Task<Void> channelsTask = new Task<>() {
+            @Override
+            protected Void call() {
+                // service.getChannels();
+                service.initializeService();
+                return null;
+            }
+        };
+        channelsTask.setOnSucceeded(e -> {
+            if (homeView.isShowing()) {
+                final double width = homeView.getScene().getWidth();
+                changeOrientation(width);
+            }
+        });
+        Thread th = new Thread(channelsTask);
+        th.setDaemon(true);
+        th.start();
+        final VBox vBox = new VBox(10, new ProgressIndicator(), new Label("Retrieving contacts.."));
+        vBox.setAlignment(Pos.CENTER);
+        homeView.setCenter(new StackPane(vBox));
     }
 }
