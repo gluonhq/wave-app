91cdbc11475c99df97021177d4752259e2c11c43
jose.pereda <jose.pereda@gluonhq.com>
2022-11-21 13:20:58 +0100

    Use codec to keep emoji unicode and not in realGetText

--- EmojiTextArea/src/main/java/com/gluonhq/emoji/impl/skin/LinkedEmoji.java
+++ EmojiTextArea/src/main/java/com/gluonhq/emoji/impl/skin/LinkedEmoji.java
@@ -1,8 +1,13 @@
 package com.gluonhq.emoji.impl.skin;
 
 import com.gluonhq.emoji.Emoji;
+import com.gluonhq.emoji.EmojiData;
 import javafx.scene.Node;
+import org.fxmisc.richtext.model.Codec;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.Optional;
 
 public interface LinkedEmoji {
@@ -11,5 +16,37 @@ public interface LinkedEmoji {
     
     Optional<Emoji> getEmoji();
 
+    String getEmojiData();
+
     Node createNode();
+
+    Codec<LinkedEmoji> EMOJI_CODEC = new Codec<>() {
+
+        @Override
+        public String getName() {
+            return "linkedEmoji";
+        }
+
+        @Override
+        public void encode(DataOutputStream os, LinkedEmoji linkedEmoji) throws IOException {
+            if (linkedEmoji.isReal()) {
+                os.writeBoolean(true);
+                Codec.STRING_CODEC.encode(os, linkedEmoji.getEmojiData());
+            } else {
+                os.writeBoolean(false);
+            }
+        }
+
+        @Override
+        public LinkedEmoji decode(DataInputStream is) throws IOException {
+            if (is.readBoolean()) {
+                String emojiData = Codec.STRING_CODEC.decode(is);
+                return EmojiData.emojiFromCodepoints(emojiData)
+                        .map(emoji -> ((LinkedEmoji) new RealLinkedEmoji(emoji)))
+                        .orElse(new EmptyLinkedEmoji());
+            } else {
+                return new EmptyLinkedEmoji();
+            }
+        }
+    };
 }
